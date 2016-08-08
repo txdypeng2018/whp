76a4544f86f3d5f2c58d93330c7f9cc4e097e10f
propersoft-ci <hexin@propersoft.cn>
2016-08-08 08:35:47 +0800

    Propersoft CI synchronized deploy branch at Mon Aug  8 08:35:47 CST 2016

---
+++ proxy/modules/register/registration/handler.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var common = require('../../common.js');
+
+var handler = module.exports = {};
+
+handler.onGet = function(req, res) {
+  common.jsonRes(req, res, '/all');
+};
+
+handler.onPut = function(req, res, data) {
+  console.info(data);
+  res.setHeader('Content-Type', 'application/json');
+  res.write(JSON.stringify({ id: '1' }));
+  res.statusCode = 200;
+  res.end();
+};
