0b77f96ad7b9a40f59f889aa75c352a39f7a86e9
李晨熙 <lichenxi@propersoft.cn>
2016-08-22 17:09:33 +0800

    学科选择页面

--- src/app/components/spinner.js
+++ src/app/components/spinner.js
@@ -8,8 +8,9 @@ angular.module('isj').directive('isjSpinner', function() {
       isShow: '=isShow'
     },
     template: '',
-    controller:function($scope, ngProgressFactory) {
+    controller:function($scope, $element, ngProgressFactory) {
       $scope.progressbar = ngProgressFactory.createInstance();
+      $scope.progressbar.setParent($element[0]);
       $scope.progressbar.setColor('#387ef5');
       $scope.$watch('isShow', function (newValue, oldValue) {
         if (newValue) {
--- src/modules/subject/subjectSelect.js
+++ src/modules/subject/subjectSelect.js
@@ -1,41 +1,51 @@
 (function(app) {
   'use strict';
 
-  var subjectSelectCtrl = function($scope, $http, $state, $stateParams, $timeout) {
+  var subjectSelectCtrl = function($scope, $http, $state, $stateParams, $timeout, $ionicHistory) {
+    $scope.hideSearch = true;
+    $scope.type = $stateParams.type;
+    $scope.districtId = '';
+    $scope.subjectId = '';
+
     //取得学科列表
     var getSubjects = function() {
       $http.get('/subjects', {params: {districtId: $scope.districtId}}).success(function(data) {
         $scope.subjects = data;
 
         //默认选中第一个一级学科
-        $timeout(function(){
-          angular.element(document.querySelectorAll('.subject-div-left')[0]).addClass('left-activated');
-          if (angular.isUndefined(data[0].subjects) || data[0].subjects.length === 0) {
-            $scope.subjectRights = [data[0]];
-          }
-          else {
-            $scope.subjectRights = data[0].subjects;
-          }
-        });
+        $scope.subjectId = data[0].id;
+        if (angular.isUndefined(data[0].subjects) || data[0].subjects.length === 0) {
+          $scope.subjectRights = [data[0]];
+        }
+        else {
+          $scope.subjectRights = data[0].subjects;
+        }
       });
     };
 
     //取得院区信息
     $http.get('/organization/district').success(function(data) {
-      $scope.districts = data;
+      $scope.districts = [{id:'', name:'全部'}];
+      for (var i = 0 ; i < data.length ; i++) {
+        data[i].name = data[i].name + '院区';
+        $scope.districts.push(data[i]);
+      }
+
       getSubjects();
     });
 
+    //返回上页
+    $scope.goBack = function() {
+      $ionicHistory.goBack();
+    };
+
     //查询框显示隐藏事件
     $scope.searchClk = function() {
-      if (angular.element(document.querySelector('.head-search')).hasClass('search-none')) {
-        angular.element(document.querySelector('.head-search')).removeClass('search-none');
+      $scope.hideSearch = !$scope.hideSearch;
+      if (!$scope.hideSearch) {
         $timeout(function(){
-          document.getElementById('subjectSelect_search').focus();
-        });
-      }
-      else {
-        angular.element(document.querySelector('.head-search')).addClass('search-none');
+          document.getElementById('subjectSelect_search_'+$scope.type).focus();
+        }, 50);
       }
     };
 
@@ -52,59 +62,46 @@
     };
 
     //院区选择事件
-    $scope.districtClk = function(event, id) {
-      $scope.districtId = id;
-      var districtDocuments = document.querySelectorAll('.district-button');
-      if (!angular.element(event.currentTarget).hasClass('button-outline')) {
-        angular.element(event.currentTarget).addClass('button-outline');
-        $scope.districtId = '';
+    $scope.districtClk = function(id) {
+      if ($scope.districtId !== id) {
+        $scope.districtId = id;
+        getSubjects();
       }
-      else {
-        for (var i = 0 ; i < districtDocuments.length ; i++) {
-          if (!angular.element(districtDocuments[i]).hasClass('button-outline')) {
-            angular.element(districtDocuments[i]).addClass('button-outline');
-          }
-        }
-        angular.element(event.currentTarget).removeClass('button-outline');
-      }
-
-      getSubjects();
     };
 
     //左侧一级学科选择事件
-    $scope.subjectLeftClk = function(event, id) {
-      angular.element(document.querySelectorAll('.subject-div-left')).removeClass('left-activated');
-      angular.element(event.currentTarget).addClass('left-activated');
-      for (var i = 0 ; i < $scope.subjects.length ; i++) {
-        if ($scope.subjects[i].id === id) {
-          if (angular.isUndefined($scope.subjects[i].subjects) || $scope.subjects[i].subjects.length === 0) {
-            $scope.subjectRights = [$scope.subjects[i]];
+    $scope.subjectLeftClk = function(id) {
+      if ($scope.subjectId !== id) {
+        $scope.subjectId = id;
+
+        for (var i = 0 ; i < $scope.subjects.length ; i++) {
+          if ($scope.subjects[i].id === id) {
+            if (angular.isUndefined($scope.subjects[i].subjects) || $scope.subjects[i].subjects.length === 0) {
+              $scope.subjectRights = [$scope.subjects[i]];
+            }
+            else {
+              $scope.subjectRights = $scope.subjects[i].subjects;
+            }
+            break;
           }
-          else {
-            $scope.subjectRights = $scope.subjects[i].subjects;
-          }
-          break;
         }
       }
     };
 
     //右侧一级科室选择事件
     $scope.subjectRightClk = function(id) {
-      $timeout(function(){
-        if ($stateParams.type === '1') {
-          $state.go('registerTodayDoctorList', {districtId: $scope.districtId, subjectId: id});
-        }
-        else if ($stateParams.type === '2') {
-          $state.go('registerDoctorDateSelect', {districtId: $scope.districtId, subjectId: id});
-        }
-      }, 10);
+      if ($stateParams.type === '1') {
+        $state.go('registerTodayDoctorList', {districtId: $scope.districtId, subjectId: id});
+      }
+      else if ($stateParams.type === '2') {
+        $state.go('registerDoctorDateSelect', {districtId: $scope.districtId, subjectId: id});
+      }
     };
   };
 
   var mainRouter = function($stateProvider) {
     $stateProvider.state('subjectSelect', {
       url: '/subject/subjectSelect/:type',
-      cache: 'false',
       templateUrl: 'modules/subject/subjectSelect.html',
       controller: subjectSelectCtrl
     });
