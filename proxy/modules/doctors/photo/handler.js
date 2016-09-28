e3864168ec879b4056945ac561cfc892ed3334de
propersoft-ci <hexin@propersoft.cn>
2016-09-28 15:57:26 +0800

    Propersoft CI synchronized deploy branch at Wed Sep 28 15:57:26 CST 2016

--- proxy/modules/doctors/photo/handler.js
+++ proxy/modules/doctors/photo/handler.js
@@ -9,17 +9,17 @@ handler.onGet = function(req, res) {
   if (query.doctorId === '3') {
     var Module = require('module');
     var fs = require('fs');
-    Module._extensions['.png'] = function(module, fn) {
+    Module._extensions['.jpg'] = function(module, fn) {
       var base64 = fs.readFileSync(fn).toString('base64');
       module._compile('module.exports="data:image/png;base64,' + base64 + '"', fn);
     };
-    var image = require('./photo.png');
-    res.writeHead(200, {'Content-Type': 'image/png'});
+    var image = require('./photo.jpg');
+    res.writeHead(200, {'Content-Type': 'image/jpg'});
     res.write(image, 'binary');
     res.end();
   }
   else {
-    res.writeHead(200, {'Content-Type': 'image/png'});
+    res.writeHead(200, {'Content-Type': 'image/jpg'});
     res.write('', 'binary');
     res.end();
   }
