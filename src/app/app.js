adbab7d1984a6f6f4d28db66a837221adf0c4c79
李晨熙 <lichenxi@propersoft.cn>
2016-10-18 10:02:29 +0800

    显示ios的keyboardAccessoryBar

--- src/app/app.js
+++ src/app/app.js
@@ -14,7 +14,7 @@
 		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
 		// for form inputs)
 		if($window.cordova && $window.cordova.plugins && $window.cordova.plugins.Keyboard) {
-			$window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+			$window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
 			$window.cordova.plugins.Keyboard.disableScroll(true);
 		}
 		if($window.StatusBar) {
