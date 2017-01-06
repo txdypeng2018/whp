2b6d17e34f796638f6a3cf6113881db65cec63f6
AlphaHinex <AlphaHinex@gmail.com>
2017-01-06 16:18:25 +0800

    Only users with role could login

--- src/modules/auth/login/login.js
+++ src/modules/auth/login/login.js
@@ -1,22 +1,33 @@
 (function(app) {
   'use strict';
 
-  var loginCtrl = function($scope, $http, $document, $location, $window, $log) {
+  var loginErr = function($scope, $window) {
+    $scope.logging = false;
+    $scope.loginErr = true;
+    $scope.user = {};
+    document.getElementById('uname').focus();
+    delete $window.localStorage.token;
+  };
+
+  var loginCtrl = function($scope, $http, $location, $window, $log) {
     $scope.login = function() {
       $scope.logging = true;
       $http.post('/auth/login', $scope.user).then(
         function(response) {
-          $scope.logging = false;
-          $scope.loginErr = false;
-          $window.localStorage.token = response.data;
-          $location.url('/');
+          var token = response.data;
+          var payload = atob(token.split('.')[1]);
+          if (JSON.parse(payload).hasRole) {
+            $scope.logging = false;
+            $scope.loginErr = false;
+            $window.localStorage.token = token;
+            $location.url('/');
+          } else {
+            $log.debug('User has no role!');
+            loginErr($scope, $window);
+          }
         }, function(response) {
           $log.debug(response);
-          $scope.logging = false;
-          $scope.loginErr = true;
-          $scope.user = {};
-          document.getElementById('uname').focus();
-          delete $window.localStorage.token;
+          loginErr($scope, $window);
         }
       );
     };
