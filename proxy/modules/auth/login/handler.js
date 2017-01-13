7394007609fbd2896a515024c9fd12345a98be73
propersoft-ci <hexin@propersoft.cn>
2017-01-13 13:15:48 +0800

    Propersoft CI synchronized fe-deploy branch at Fri Jan 13 13:15:48 CST 2017

--- proxy/modules/auth/login/handler.js
+++ proxy/modules/auth/login/handler.js
@@ -6,7 +6,7 @@ handler.onPost = function(req, res, data) {
   var username = data.username;
   if (username === 'admin') {
     res.statusCode = 200;
-    res.write('eyJpZCI6ImRjNjU3NjZjLTAxNzYtNGExZS1hZDBlLWRkMDZiYTY0NWM3bCIsIm5hbWUiOiJhZG1pbiJ9.eyJlbXBOYW1lIjpudWxsLCJyb2xlcyI6ImEsYixjIn0.jgFLzfaFjADFUfti3jGMNqhYb1KN1anU8OkXKh3uKwk');
+    res.write('eyJpZCI6ImRjNjU3NjZjLTAxNzYtNGExZS1hZDBlLWRkMDZiYTY0NWM3bCIsIm5hbWUiOiJhZG1pbiJ9.eyJoYXNSb2xlIjp0cnVlfQ.jgFLzfaFjADFUfti3jGMNqhYb1KN1anU8OkXKh3uKwk');
     res.end();
   } else {
     res.statusCode = 401;
