77f636c6a9d033611ba2e651652cf79c2352b093
AlphaHinex <AlphaHinex@gmail.com>
2017-01-24 15:04:40 +0800

    Add role and menu

--- src/main/resources/sql/roles-menus.sql
+++ src/main/resources/sql/roles-menus.sql
@@ -1,10 +1,11 @@
 INSERT INTO pep_auth_roles_menus (role_id, menu_id) VALUES ('role-kf', 'app-yjfk');
+INSERT INTO pep_auth_roles_menus (role_id, menu_id) VALUES ('role-kf', 'app-ywtj');
 INSERT INTO pep_auth_roles_menus (role_id, menu_id) VALUES ('role-xxwh', 'app-wxts');
 INSERT INTO pep_auth_roles_menus (role_id, menu_id) VALUES ('role-xxwh', 'app-yydh');
 INSERT INTO pep_auth_roles_menus (role_id, menu_id) VALUES ('role-xxwh', 'app-lbtp');
 INSERT INTO pep_auth_roles_menus (role_id, menu_id) VALUES ('role-xxwh', 'app-ghgz');
-INSERT INTO pep_auth_roles_menus (role_id, menu_id) VALUES ('role-kf', 'app-ywtj');
 INSERT INTO pep_auth_roles_menus (role_id, menu_id) VALUES ('role-xxwh', 'app-ywtj');
 INSERT INTO pep_auth_roles_menus (role_id, menu_id) VALUES ('role-xxwh', 'app-apppz');
-INSERT INTO pep_auth_roles_menus (role_id, menu_id) VALUES ('role-xxwh', 'app-tomcat-log');
+INSERT INTO pep_auth_roles_menus (role_id, menu_id) VALUES ('role-dev', 'app-tomcat-log');
+INSERT INTO pep_auth_roles_menus (role_id, menu_id) VALUES ('role-dev', 'app-logs');
 COMMIT;
