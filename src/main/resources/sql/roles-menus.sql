f31ed83c586b38f83bfb3de66d1fd8ade7e04f15
李晨熙 <lichenxi@propersoft.cn>
2017-02-09 16:50:37 +0800

    Menus内添加客服查询 (#234)

--- src/main/resources/sql/roles-menus.sql
+++ src/main/resources/sql/roles-menus.sql
@@ -1,5 +1,6 @@
 INSERT INTO pep_auth_roles_menus (role_id, menu_id) VALUES ('role-kf', 'app-yjfk');
 INSERT INTO pep_auth_roles_menus (role_id, menu_id) VALUES ('role-kf', 'app-ywtj');
+INSERT INTO pep_auth_roles_menus (role_id, menu_id) VALUES ('role-kf', 'app-kfcx');
 INSERT INTO pep_auth_roles_menus (role_id, menu_id) VALUES ('role-xxwh', 'app-wxts');
 INSERT INTO pep_auth_roles_menus (role_id, menu_id) VALUES ('role-xxwh', 'app-yydh');
 INSERT INTO pep_auth_roles_menus (role_id, menu_id) VALUES ('role-xxwh', 'app-lbtp');
