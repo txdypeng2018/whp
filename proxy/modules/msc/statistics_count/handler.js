b074311b2d88914abda799e6fc691583bb8b8190
propersoft-ci <hexin@propersoft.cn>
2017-02-13 11:10:52 +0800

    Propersoft CI synchronized fe-deploy branch at Mon Feb 13 11:10:52 CST 2017

---
+++ proxy/modules/msc/statistics_count/handler.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2016/12/20.
+ */
+'use strict';
+var common = require('../../common.js');
+
+var handler = module.exports = {};
+
+handler.onGet = function(req, res) {
+    common.jsonRes(req, res, '/all');
+};
