36afe12850dd3806eb3c26b033e39fdf8e6aa6f1
eastoneking <wangds2016@163.com>
2017-02-23 23:51:50 +0800

    Zssj 496 simple 延时退款-缴费 (#255)
    * user pay jar
    
    * fix
    
    * recipe pay
    
    * 参照ZSSJ_496,移除重构部分，保留日志部分。
    
    * 延时退分定时任务处理.
    
    * 整理代码:checkstyle和findebugs
    
    * 修正遗留的处理状态
    
    * 优化延时退款流程<br/>RegisterController中的占号点方法移植到Service中;<br/>RegistrationService中添加占号点方法,增加sendRegistrationMsg方法;<br/>RegistrationServiceNotxImpl中sendRegistrationMsg改为public,添加占号点方法.
    
    * 根据要求重新调整代码格式.
    
    * 重新格式化所有修改的类
    
    * 补充checkstyle中的tab检查。
    
    * 根据teamcity日志处理tab字符问题
    
    * 修正DefaultPayLogRecord的命名，改为DefaultPayLogRecordEntity
    
    * 调整task配置初始化;<br/>简化xml配置;<br/>格式化配置文件;<br/>移除AbstractLoggerTest.groovy.
    
    * 补充缴费延时退款。

---
+++ src/main/java/com/proper/enterprise/isj/payment/converter/delay/Order2PayLogRecordConverter.java
@@ -0,0 +1,20 @@
+package com.proper.enterprise.isj.payment.converter.delay;
+
+import com.proper.enterprise.isj.order.model.Order;
+import com.proper.enterprise.isj.payment.converter.AbstractManagedConverter;
+import com.proper.enterprise.isj.payment.logger.entity.PayLogRecord;
+
+public class Order2PayLogRecordConverter extends AbstractManagedConverter<Class, Order, PayLogRecord> {
+
+    @Override
+    public PayLogRecord convert(Order source, PayLogRecord target) {
+        target.setOrderId(source.getOrderNo());
+        target.setPayWay(source.getPayWay());
+        target.setRefundAmount("0");
+        target.setRefundWay("");
+        target.setTotalFee(source.getOrderAmount());
+        target.setUserId(source.getLastModifyUserId());
+        return target;
+    }
+
+}
