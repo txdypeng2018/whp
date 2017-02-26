2bdba6ae0c1c95aba6b0bfb545436dee982cc358
propersoft-ci <hexin@propersoft.cn>
2017-02-26 15:54:33 +0800

    Propersoft CI synchronized fe-deploy branch at Sun Feb 26 15:54:33 CST 2017

--- proxy/modules/msc/service_user_opinion/handler.js
+++ proxy/modules/msc/service_user_opinion/handler.js
@@ -6,9 +6,9 @@ var handler = module.exports = {};
 
 handler.onGet = function(req, res) {
   var query = common.parseUrl(req).query;
-  console.log(query.query.length);
-  if (query.query.length > 40) {
-    common.jsonRes(req, res, '/user_2');
+  console.log(query.query);
+  if (query.query !== '{}') {
+    common.jsonRes(req, res, '/user_1');
   }
   else{
     common.jsonRes(req, res, '/all');
