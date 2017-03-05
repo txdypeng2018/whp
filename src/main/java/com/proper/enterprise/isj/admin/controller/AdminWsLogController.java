7b104829d548fa01cedac43f9663f12f178eedc8
李晨熙 <lichenxi@propersoft.cn>
2017-03-05 19:51:08 +0800

    wslog查询接口 (#271)
    * 中间版本
    
    * wslog查询接口
    
    * 修改问题

---
+++ src/main/java/com/proper/enterprise/isj/admin/controller/AdminWsLogController.java
@@ -0,0 +1,30 @@
+package com.proper.enterprise.isj.admin.controller;
+
+import com.proper.enterprise.isj.log.document.WSLogDocument;
+import com.proper.enterprise.isj.log.service.WSLogService;
+import com.proper.enterprise.platform.core.controller.BaseController;
+import com.proper.enterprise.platform.core.entity.DataTrunk;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+@RequestMapping("/logview")
+public class AdminWsLogController extends BaseController {
+
+    @Autowired
+    private WSLogService wSLogService;
+
+    /**
+     * 取得人员信息列表
+     */
+    @GetMapping(path = "/wsLog")
+    public ResponseEntity<DataTrunk<WSLogDocument>> wsLog(int pageNo, int pageSize,
+            String search, String startDate, String endDate) throws Exception {
+        DataTrunk<WSLogDocument> dataTrunk = wSLogService.getWsLogList(pageNo, pageSize, search,
+                startDate, endDate);
+        return responseOfGet(dataTrunk);
+    }
+}
--- src/main/java/com/proper/enterprise/isj/log/service/WSLogService.java
+++ src/main/java/com/proper/enterprise/isj/log/service/WSLogService.java
@@ -2,13 +2,24 @@ package com.proper.enterprise.isj.log.service;
 
 import com.proper.enterprise.isj.log.document.WSLogDocument;
 import com.proper.enterprise.isj.log.repository.WSLogRepository;
+import com.proper.enterprise.platform.core.entity.DataTrunk;
+import com.proper.enterprise.platform.core.utils.StringUtil;
+import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 @Service
 public class WSLogService {
@@ -17,6 +28,9 @@ public class WSLogService {
 
     @Autowired
     private WSLogRepository repository;
+
+    @Autowired
+    MongoTemplate mongoTemplate;
 
     @Async
     public void persistLog(String methodName, Map<String, Object> param,
@@ -33,6 +47,31 @@ public class WSLogService {
         repository.save(new WSLogDocument(methodName, param, req, res, duration));
     }
 
+    public DataTrunk<WSLogDocument> getWsLogList(int pageNo, int pageSize, String search,
+            String startDate, String endDate) throws Exception {
+        String startObjectId = convertTimeToObjectId(startDate);
+        String endObjectId = convertTimeToObjectId(endDate);
+        Query query = new Query();
+        if (StringUtil.isNotEmpty(search)) {
+            Pattern ptSearch = Pattern.compile("^.*"+search+".*$", Pattern.CASE_INSENSITIVE);
+            query.addCriteria(new Criteria().orOperator(Criteria.where("methodName").regex(ptSearch),
+                    Criteria.where("req").regex(ptSearch), Criteria.where("res").regex(ptSearch)));
+        }
+        if (StringUtil.isNotEmpty(startDate) && StringUtil.isNotEmpty(endDate)) {
+            query.addCriteria(Criteria.where("id").gte(new ObjectId(startObjectId)).lte(new ObjectId(endObjectId)));
+        } else if (StringUtil.isNotEmpty(startDate) && StringUtil.isEmpty(endDate)) {
+            query.addCriteria(Criteria.where("id").gte(new ObjectId(startObjectId)));
+        } else if (StringUtil.isEmpty(startDate) && StringUtil.isNotEmpty(endDate)) {
+            query.addCriteria(Criteria.where("id").lte(new ObjectId(endObjectId)));
+        }
+        long count = mongoTemplate.count(query, WSLogDocument.class);
+        query.skip((pageNo - 1) * pageSize);
+        query.limit(pageSize);
+        query.with(new Sort(new Sort.Order(Sort.Direction.DESC, "createTime")));
+        List<WSLogDocument> logs = mongoTemplate.find(query, WSLogDocument.class);
+        return new DataTrunk<WSLogDocument>(logs, count);
+    }
+
     private String traceThrowable(Throwable t) {
         if (t == null) {
             return "";
@@ -45,4 +84,15 @@ public class WSLogService {
         return sb.toString();
     }
 
+    private String convertTimeToObjectId(String time) throws Exception {
+        String objectId = "";
+        if (StringUtil.isNotEmpty(time)) {
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date date= df.parse(time);
+            Long seconds = date.getTime()/1000;
+            String secondHex = seconds.toHexString(seconds);
+            objectId = secondHex+"0000000000000000";
+        }
+        return objectId;
+    }
 }
