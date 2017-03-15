a1e307f580b9aa1b6d2a2bc596c00f44cd3ad0b9
王维 <ww.gggxwy@163.com>
2017-03-15 13:33:38 +0800

    nginx conf (#295)
    * nginx conf
    
    * Update docker-compose.yml
    
    * Update update.sh
    
    * Update update.sh
    
    * Update update.sh
    
    * Update nginx.conf
    
    * Update nginx.conf
    
    * Update nginx.conf
    
    * Fix
    
    * Before test
    
    * Adjust
    
    * Adjust
    
    * git st
    
    * Update README

--- 服务器106/opt/docker/isj_official/nginx/hc/README.md
+++ 服务器106/opt/docker/isj_official/nginx/hc/README.md
@@ -2,5 +2,8 @@ APP 热部署服务
 ============
 
 1. 从 TeamCity 中找到要部署版本的构建成果物（Artifacts），点击进入 `Artifacts` 界面后，查看 URL 地址，从中得到 `buildId` 的参数
-1. 在服务器相应路径中，执行 `$ ./update.sh $buildId` 即可完成新版的发布
-1. 发布完成后，可通过 [https://sjh.sj-hospital.org/hc/chcp.json](https://sjh.sj-hospital.org/hc/chcp.json) 进行检验。**若未生效，需重启容器使生效**。
+1. 在服务器相应路径中，执行 `$ ./update.sh $buildId` 即可完成 **debug** 版的发布
+> 注意：如果要进行新版的**正式发布**，需增加 `--release` 参数，如 `$ ./update.sh $buildId --release`
+1. 发布完成后，可通过如下地址进行检验。**若未生效，需重启容器使生效**。
+    * debug 版：[https://sjh.sj-hospital.org/hc_debug/chcp.json](https://sjh.sj-hospital.org/hc_debug/chcp.json)
+    * 正式版： [https://sjh.sj-hospital.org/hc/chcp.json](https://sjh.sj-hospital.org/hc/chcp.json)
--- 服务器106/opt/docker/isj_official/nginx/hc/update.sh
+++ 服务器106/opt/docker/isj_official/nginx/hc/update.sh
@@ -5,9 +5,28 @@ then
     exit 1;
 fi
 
-export WORKDIR=/opt/docker/isj_official/nginx/hc
+WORKDIR=/opt/docker/isj_official/nginx/hc
+HC_HOME=/www_debug
+HC_BAK_HOME=/www_debug
+TC_HOME=Ihos_APPDebug
+
+if [ "$2" == "--release" ];
+then
+    HC_HOME=/www
+    HC_BAK_HOME=/www_bak
+    TC_HOME=Ihos_Build
+fi
+
 cd $WORKDIR
-curl -O https://server.propersoft.cn/teamcities/guestAuth/repository/downloadAll/Ihos_Build/$1:id/artifacts.zip
-rm -rf $WORKDIR/wwwbak
-mv $WORKDIR/www $WORKDIR/wwwbak
-unzip $WORKDIR/artifacts.zip -d $WORKDIR/www -x index.html *.apk
+URL=https://server.propersoft.cn/teamcities/guestAuth/repository/downloadAll/$TC_HOME/$1:id/artifacts.zip
+echo 'Downloading from '$URL
+curl -O $URL
+if [ -e $WORKDIR/artifacts.zip ];
+then
+    rm -rf $WORKDIR$HC_BAK_HOME
+    mv $WORKDIR$HC_HOME $WORKDIR$HC_BAK_HOME
+    echo 'Unziping artifacts.zip...'
+    unzip -qq $WORKDIR/artifacts.zip -d $WORKDIR$HC_HOME -x index.html *.apk
+    rm -f $WORKDIR/artifacts.zip
+    echo 'Done'
+fi
