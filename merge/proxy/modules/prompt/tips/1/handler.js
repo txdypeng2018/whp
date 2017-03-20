9d6d96ae64a5fd364dd7e6b6ee60d83aa368cdb3
propersoft-ci <hexin@propersoft.cn>
2017-03-20 09:20:47 +0800

    Propersoft CI synchronized fe-deploy merge branch at Mon Mar 20 09:20:47 CST 2017

---
+++ merge/proxy/modules/prompt/tips/1/handler.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var common = require('../../../../common.js');
+
+var handler = module.exports = {};
+
+handler.onGet = function(req, res) {
+  common.jsonRes(req, res, '/all');
+};
+
+
